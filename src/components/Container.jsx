aea33800d67e1e0d1a6e456e2eae3f4e977761f0
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-26 10:57:02 +0700

    update Container component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,9 +1,9 @@
 import { classNames } from "../utils";
 
-export default function Container({ children, maxWidthClass, ...props }) {
+export default function Container({ children, maxWidthClass }) {
   maxWidthClass = maxWidthClass || "max-w-5xl";
   return (
-    <div className={classNames(maxWidthClass, "mx-auto px-4 sm:px-6")}>
+    <div className={classNames(maxWidthClass, "mx-auto px-5 sm:px-8")}>
       {children}
     </div>
   );
