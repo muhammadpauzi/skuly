ef28fae91c3037a0ede859b913f82ee707a46884
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-27 08:22:35 +0700

    add search field, add ClassCard component and update Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,18 @@
 import { classNames } from "../utils";
 
-export default function Input({ className, ...props }) {
+export default function Input({ styleClassName, className, ...props }) {
+  const baseStyleClassName = classNames(
+    className ? className : "",
+    "appearance-none rounded-md relative block w-full px-3 py-2.5 focus:z-10 sm:text-sm transition"
+  );
+  styleClassName =
+    styleClassName ||
+    "border-2 border-gray-200 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-orange-500 focus:border-orange-500";
+
   return (
     <input
       {...props}
-      className={classNames(
-        className ? className : "",
-        "appearance-none rounded-md relative block w-full px-3 py-2.5 border-2 border-gray-200 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-orange-500 focus:border-orange-500 focus:z-10 sm:text-sm"
-      )}
+      className={classNames(baseStyleClassName, styleClassName)}
     />
   );
 }
