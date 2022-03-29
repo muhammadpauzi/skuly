d5476a74ac069d7df1f346301098d56f7d1c7160
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-03-29 17:43:35 +0700

    fix: baseUrl

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.API_BASE_URL;
 export default axios.create({
-    baseURL: API_BASE_URL + '/api/v1',
+    baseURL: `${API_BASE_URL}/api/v1`,
     headers: {
         // "Content-Type": "application/x-www-form-urlencoded",
         'X-Requested-With': 'XMLHttpRequest',
